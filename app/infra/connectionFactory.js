74d5156c6305685640165667acedb843d5bc8d84
Claudio Stevanato <cstevanato@gmail.com>
2018-04-15 18:04:44 -0300

    conexao heroku

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -9,13 +9,23 @@ function createDBConnection() {
             database: 'casadocodigo_nodejs'
         });
     }
-
     if(process.env.NODE_ENV == 'test') {
         return mysql.createConnection({
             host: 'localhost',
             user: 'root',
             password: '12345',
             database: 'casadocodigo_nodejs_test'
+        });
+    }
+
+    if(process.env.NODE_ENV == 'production') {
+        var urlDeConexao = process.env.CLEARDB_DATABASE_URL;
+        var grupos = urlDeConexao.match(/mysql:\/\/(.*)):(.*)@(.*)\/(.*)\?reconnect=true/)
+        return mysql.createConnection({
+            host: grupos[3],
+            user: grupos[4],
+            password: grupos[5],
+            database: grupos[6]
         });
     }
 }
